31a991359c23b1e52510064a69ec4d0f7e63c813
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-04-16 11:19:04 +0100

    Add YLYD

---
+++ assets/js/youlaughyoudrink.js
@@ -0,0 +1,83 @@
+text = `Act like a DJ controlling electrodes attached to their nipples
+Gets as physically close to someone without touching them
+Acts like a very flamboyant butterfly hatching out of a cocoon
+You're a pirate, and your map shows where the buried treasure is. It's in your Target's pants
+You're a hummingbird. Flap your little wings and go sip some nectar out of the juiciest tower (your Target)
+Make eye contact with your Target and lick every inch of your lips
+Ask the Target whcih of the following is their favorite cereal/drug combination: Kashi and Ketamine, Muesli and Meth, Apple Jacks and smokin' Crack, Corn Flakes and bath salts, CTC and PCP
+You're Nicolas Cage, and the Target is the Decleration of Independence
+You're a Tickle Me Elmo. Insist that the Target tickle you, and if they do, get furious at them
+Get emotional and tell your Target, "I don't know how to say this, but I'm sexually attracted to you and I want to wear your skin"
+You're a dolphin, slap your little fins together and try to find your Target with echolocation
+Dance using only your hips
+Yell "How dare you!" at the Target, then slap yourself
+Mime a queef
+Announce that this is not part of the game. Get down on one knee and propose to your Target. Include details of everything you love about them
+In your best Obama voice, say to the Target, "I'm looking for a dime that's top of the line: Cute face, slim waist, with a big behind"
+Do your best impression of Mason Ramsey yodeling "Lovesick Blues" in a Walmart
+In a terrible singing voice, sing "Hey There Delilah" but substitute your Target's name for "Delilah"
+Make a very loud bird sound, fly over to your Target, and start pecking at them
+Demonstrate to the class how to put on a condom, using the Target's head as the tip of the penis
+Show your Target how to swing a golf club by standing right behind them and grabbing their hands
+You are a stork dropping off a heavy-ass baby to a mom (the Target)
+You're a cowboy. Go around the group and find the most trusty steed you can (your Target). Then mount up - we ride!
+Leave the room, come back, look right at the Target, sigh, and then immediately leave again
+You're a therapist, and your Target's crotch is your deeply troubled patient
+Do an impersonation of yourself
+Do a PSA on why sex is bad and scary
+Ask your Target to get on all fours. Pretend to milk them like a cow, explaining the process aloud and describing the quality of the milk they are producing
+Do a very shy Magic Mike dance for your Target
+Your Target is a turkey you're about to cook. Explain the proper cooking technique to your TV-show audience. You'll prepare the turkey by "tying it up and bitch slapping it with some butter"
+You're Mickey Mouse going through withdrawals, and your Target has the drugs you're looking for
+You're a sushi chef berating the Target for using a fork in your restaurant
+Lay on the floor underneath your Target and say to the group "Hey, my name is Steve-O, and this is the nut wrangler"
+Talk sexy to your drink. Tell it you crave its condensation and thirst-quenching
+Spin in circles, yelling "I AM A WASHING MACHINE!" repeatedly
+You're a chubby seal, and the Target just got too close to your seal pup. Bark at them to scare them off
+Try to do the splits while screaming in agony and looking right at your Target
+Have an emotional breakdown while doing some dad dance moves
+Act as if you are about to perform a bris on your Target
+You're a buyer at a cattle auction, and the Target is just about the most delightful steer you've ever seen
+Look at the Target like they are a mirror and you're getting ready in the morning. Give yourself compliments: "Wow, I look fucking good." "I would definitely have sex with myself."
+You are a magnet. Your Target is a giant piece of metal. Yell "I'M A MAGNET" as you hurtle across the room.
+Ask the group to close their eyes, bow their heads and beg that your Target might finally "get that dick"
+Blow a line of coke off your Target's crotch
+Hold a squat while maintaining eye contact with your Target
+Do some gymnastic moves while yelling "Look at me, Daddy!" to your Target
+Mime giving a nice thorough hand job - no speaking allowed (grunting is acceptable)
+Writhe on the ground, yelling to your Target, "Please help me - I am but a humble fish!"
+Pretend you have to tell your Target a secret, then suddenly blow in their ear
+Treat the Target like a dog that wants to go for a walk
+Lightly touch parts of the Target's body while whispering what each body part is
+While motioning to the Target, say, "Welcome to The Price Is Right! The first item is this beat- ass ho. Is the price over or under $5?"
+Tell your Target you're going to give them a blow job, and then blow all over them
+Perform your own roast of the Target
+Tell the group the most emberrassing story you have about the Target
+You're drunk Santa Claus talking to his lead elf (the Target)
+Make the Target watch the first 30 seconds of the "Scatman (ski-ba-bop-da-dop-bop)" music video on YouTube
+Very gently go to the Target, place your hand on their stomach, and whisper, "Congrats, I'm glad I could put that inside of you"
+Do parkour but only immediately surrounding the Target
+In your best Hillary Clinton voice tell the Target, "I did not take those cookies"
+Narrate the sounds of both sides of a romantic encounter
+Tap it very gently on your Target's forehead
+Make a song by slapping your own ass
+☼ Sing a song about your love for your Target, using only animal sounds
+☼ Do your best impression of a televangelist and try to convert the Target to your religion
+☼ Start doing push-ups and shout "I'm gonna get swole for you, Target!"
+☼ You are an over-enthusiastic life coach and must give the Target advice on how to achieve their dreams
+☼ Act as a detective and interrogate the Target about a made-up crime
+☼ Present it as a Michelin Star dish, and ask the Target to taste your package
+☼ Pretend to be a toddler having a temper tantrum because the Target won't give you what you want
+☼ Do your best impression of a game show host and ask the Target to compete in an imaginary game show
+☼ Pretend to be a mime walking against a strong wind, while trying to get to the Target
+☼ Sell honey to the Target, turned on
+☼ Pretend to be a news anchor reporting on a ridiculous story and interview the Target about their involvement
+☼ Ask the Target to be your dance teacher and follow their every move
+☼ Pretend to be a cat and rub against your Target's leg, purring loudly
+☼ You're a yoga instructor and your Target is a new student. Take them through a relaxing yoga routine`
+
+function run() {
+    var arr = text.split('\n')
+    var rand = arr[Math.floor(Math.random() * arr.length)];
+    quote.innerHTML = rand
+}
